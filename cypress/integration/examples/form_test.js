9cf9c556368b11a809d39b7e79bc49721ba535e5
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-15 17:50:26 -0400

    Ran all tests

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -1,7 +1,27 @@
 describe('MY FORM APP', () =>{
 
     beforeEach(() => {
-        crypto.visit('http://localhost:3000')
+        cy.visit('http://localhost:3000')
+    })
+
+    const nameInput = () => cy.get('input[name="name"]')
+    const emailInput = () => cy.get('input[name="email"]')
+    const passwordInput = () => cy.get('input[name="password"]')
+    const tosInput = () => cy.get('input[name="tos"]')
+
+    it('My MVP checks', () => {
+        nameInput()
+        .type('Erick')
+        .should('have.value', 'Erick')
+        emailInput()
+        .type('erickgonzalez@outlook.com')
+        passwordInput()
+        .type('password123')
+        tosInput()
+        .check()
+        .should('be.checked')
+
+
     })
 
     
